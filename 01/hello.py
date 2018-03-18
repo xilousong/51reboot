182629ad940a551f3ec6f316c5c6165ba3710c83
xilousong <xilou19@163.com>
2018-03-18 12:47:18 +0800

    add directoy 01

---
+++ 01/hello.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# 这句话是打印输出hello world 到屏幕
+
+pi = 3.1415926
+
+print("hello,world!!!")
+
+print(2 * pi * 10)
+print(pi * 10 * 10)
+print('100 + 2 =', 100 + 2)
+print('1 - 5 =', 1 - 5)
+print('5 * 6 =', 5 * 6)
+print('9 / 4 =', 9 / 4)
+
+print('hello' * 5)
+print('hello' + ',world')
+
+name = input('请输入您的名字：')
+age = input('请输入您的年龄：')
+print('我叫%s,今年%s岁'%(name,age))
